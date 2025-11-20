35279f99d42f749a6a45c986dc16aa5b6fcf5788
Mac <mehak.kushwaha@outlook.com>
2025-11-20 19:42:28 +0530

    testing

--- terraform/Practice/modules/storage/output.tf
+++ terraform/Practice/modules/storage/output.tf
@@ -19,23 +19,3 @@ output "primary_blob_endpoint" {
   value       = azurerm_storage_account.storage.primary_blob_endpoint
 }
 
-output "storage_account_id" {
-  description = "ID of the Storage Account"
-  value       = module.storage.storage_account_id
-}
-
-output "storage_account_name" {
-  description = "Name of the Storage Account"
-  value       = module.storage.storage_account_name
-}
-
-output "storage_primary_blob_endpoint" {
-  description = "Blob endpoint of the storage account"
-  value       = module.storage.primary_blob_endpoint
-}
-
-output "storage_primary_connection_string" {
-  description = "Primary connection string"
-  value       = module.storage.primary_connection_string
-  sensitive   = true
-}
