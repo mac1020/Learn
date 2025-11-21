cc3b099ed099b0f678e7efbc3a58d24eb78e0358
Mac <mehak.kushwaha@outlook.com>
2025-11-21 15:19:12 +0530

    testing

--- terraform/Practice/main.tf
+++ terraform/Practice/main.tf
@@ -19,12 +19,12 @@ module "subnet" {
   address_prefixes = var.address_prefixes
 }
 
-# module "storage" {
-#   source                = "./modules/storage"
-#   storage_account_name  = "myteststorage1234"
-#   resource_group_name   = azurerm_resource_group.rg.name
-#   location              = azurerm_resource_group.rg.location
-#   account_tier          = "Standard"
-#   replication_type      = "LRS"
-# }
-# //storage
+module "storage" {
+  source                = "./modules/storage"
+  storage_account_name  = "myteststorage1234"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  account_tier          = "Standard"
+  replication_type      = "LRS"
+}
+//storage
